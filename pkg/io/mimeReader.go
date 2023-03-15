0771053ab3c22e02a17bf823719eb67a97f0a025
Juergen Enge <github@betalabs.ch>
2023-03-15 18:41:54 +0100

    initial commit

---
+++ pkg/io/mimeReader.go
@@ -0,0 +1,42 @@
+package indexer
+
+import (
+	"emperror.dev/errors"
+	"io"
+	"net/http"
+)
+
+type MimeReader struct {
+	io.Reader
+	buffer []byte
+}
+
+func NewMimeReader(r io.Reader) (*MimeReader, error) {
+	mr := &MimeReader{
+		Reader: r,
+		buffer: make([]byte, 512),
+	}
+	return mr, mr.Init()
+}
+
+func (mr *MimeReader) Init() error {
+	n, err := mr.Reader.Read(mr.buffer)
+	if err != nil {
+		return errors.Wrap(err, "failed to read head")
+	}
+	mr.buffer = mr.buffer[:n]
+	return nil
+}
+
+func (mr *MimeReader) DetectContentType() (string, error) {
+	return http.DetectContentType(mr.buffer), nil
+}
+
+func (mr *MimeReader) Read(p []byte) (n int, err error) {
+	if len(mr.buffer) > 0 {
+		n = copy(p, mr.buffer)
+		mr.buffer = mr.buffer[n:]
+		return
+	}
+	return mr.Reader.Read(p)
+}

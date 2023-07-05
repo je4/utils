7ad2d0d00b16be6b250ba3dd2d974868aa57aed5
Juergen Enge <github@betalabs.ch>
2023-07-05 18:39:36 +0200

    use grpc type for interceptors

--- pkg/grpc/jwtInterceptor.go
+++ pkg/grpc/jwtInterceptor.go
@@ -10,24 +10,29 @@ import (
 
 var errMissingMetadata = status.Errorf(codes.InvalidArgument, "no incoming metadata in rpc context")
 var errMissingMetadataToken = status.Errorf(codes.InvalidArgument, "no token found in rpc metadata context")
+var errInvalidMetadataToken = status.Errorf(codes.InvalidArgument, "invalid token found in rpc metadata context")
 
-func JWTUnaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-	md, ok := metadata.FromIncomingContext(ctx)
-	if !ok {
-		return nil, errMissingMetadata
+func JWTUnaryTokenInterceptor(token string) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		md, ok := metadata.FromIncomingContext(ctx)
+		if !ok {
+			return nil, errMissingMetadata
+		}
+
+		tokens := md.Get("Token")
+		if len(tokens) == 0 {
+			return nil, errMissingMetadataToken
+		}
+		if tokens[0] != token {
+			return nil, errInvalidMetadataToken
+		}
+
+		md.Delete("Token")
+
+		ctx = metadata.NewIncomingContext(ctx, md)
+
+		return handler(ctx, req)
 	}
-
-	tokens := md.Get("Token")
-	if len(tokens) == 0 {
-		return nil, errMissingMetadataToken
-	}
-	//	info.FullMethod
-
-	md.Delete("Token")
-
-	ctx = metadata.NewIncomingContext(ctx, md)
-
-	return handler(ctx, req)
 }
 
 type wrappedStream struct {
@@ -39,21 +44,25 @@ func (s *wrappedStream) Context() context.Context {
 	return s.ctx
 }
 
-func JWTStreamInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-	md, ok := metadata.FromIncomingContext(ss.Context())
-	if !ok {
-		return errMissingMetadata
+func JWTStreamTokenInterceptor(token string) grpc.StreamServerInterceptor {
+	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		md, ok := metadata.FromIncomingContext(ss.Context())
+		if !ok {
+			return errMissingMetadata
+		}
+
+		tokens := md.Get("Token")
+		if len(tokens) == 0 {
+			return errMissingMetadataToken
+		}
+		if tokens[0] != token {
+			return errInvalidMetadataToken
+		}
+
+		md.Delete("Token")
+
+		ctx := metadata.NewIncomingContext(ss.Context(), md)
+
+		return handler(srv, &wrappedStream{ss, ctx})
 	}
-
-	tokens := md.Get("Token")
-	if len(tokens) == 0 {
-		return errMissingMetadataToken
-	}
-	//	info.FullMethod
-
-	md.Delete("Token")
-
-	ctx := metadata.NewIncomingContext(ss.Context(), md)
-
-	return handler(srv, &wrappedStream{ss, ctx})
 }

6495bf4bd2056a083ee91b4887870457dbbeebcc
Juergen Enge <github@betalabs.ch>
2023-07-05 18:21:29 +0200

    new scheme

--- pkg/grpc/ubResolverBuilder.go
+++ pkg/grpc/ubResolverBuilder.go
@@ -10,7 +10,7 @@ import (
 	"sync"
 )
 
-const ubResolverScheme = "static"
+const ubResolverScheme = "ubbasel"
 
 func NewUBResolver(addrs map[string][]string) *ubResolverBuilder {
 	r := &ubResolverBuilder{addrs: addrs}

d995467b9724ed2c814f567509cec3361c450cc6
Juergen Enge <github@betalabs.ch>
2023-08-18 11:48:24 +0200

    clientLimit

---
+++ pkg/http/clientLimit.go
@@ -0,0 +1,63 @@
+package http
+
+import (
+	"github.com/pkg/errors"
+	"net/http"
+	"time"
+)
+
+func NewLimitedClient(concurrency int64) *LimitedClient {
+	var lc = &LimitedClient{
+		max:    concurrency,
+		tokens: make(chan int64, concurrency),
+		client: &http.Client{},
+	}
+	for i := int64(0); i < lc.max; i++ {
+		lc.tokens <- i
+	}
+	return lc
+}
+
+type Response struct {
+	*http.Response
+	tokens chan int64
+	token  int64
+}
+
+func (resp *Response) Close() error {
+	defer func() { resp.tokens <- resp.token }()
+	return resp.Body.Close()
+}
+
+type LimitedClient struct {
+	max     int64
+	client  *http.Client
+	timeout time.Duration
+	tokens  chan int64
+}
+
+func (lc *LimitedClient) Get(urlStr string) (*Response, error) {
+	req, err := http.NewRequest(http.MethodGet, urlStr, nil)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	resp, err := lc.Do(req)
+	return resp, errors.WithStack(err)
+}
+
+func (lc *LimitedClient) Do(req *http.Request) (*Response, error) {
+	select {
+	case token := <-lc.tokens:
+		resp, err := lc.client.Do(req)
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		return &Response{
+			Response: resp,
+			tokens:   lc.tokens,
+			token:    token,
+		}, nil
+	case <-time.After(lc.timeout):
+		return nil, errors.Errorf("timeout waiting for request token")
+	}
+}
--- pkg/marc21schema/README.md
+++
@@ -1,4 +0,0 @@
-# MARC21 structure in JSON
-## by Péter Király — 2018-01-28.
-
-http://pkiraly.github.io/2018/01/28/marc21-in-json/

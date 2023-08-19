37758805f2e785eab975e2e6844d40bffb288fa3
Juergen Enge <github@betalabs.ch>
2023-08-19 15:25:41 +0200

    error corrections

--- pkg/http/clientLimit.go
+++ pkg/http/clientLimit.go
@@ -6,13 +6,14 @@ import (
 	"time"
 )
 
-func NewLimitedClient(concurrency int64, timeout time.Duration) *LimitedClient {
+func NewLimitedClient(concurrency int64, requestTimeout, tokenTimeout time.Duration) *LimitedClient {
 	var lc = &LimitedClient{
 		max:    concurrency,
 		tokens: make(chan int64, concurrency),
 		client: &http.Client{
-			Timeout: timeout,
+			Timeout: requestTimeout,
 		},
+		tokenTimeout: tokenTimeout,
 	}
 	for i := int64(0); i < lc.max; i++ {
 		lc.tokens <- i
@@ -32,10 +33,10 @@ func (resp *Response) Close() error {
 }
 
 type LimitedClient struct {
-	max     int64
-	client  *http.Client
-	timeout time.Duration
-	tokens  chan int64
+	max          int64
+	client       *http.Client
+	tokens       chan int64
+	tokenTimeout time.Duration
 }
 
 func (lc *LimitedClient) Get(urlStr string) (*Response, error) {
@@ -52,6 +53,7 @@ func (lc *LimitedClient) Do(req *http.Request) (*Response, error) {
 	case token := <-lc.tokens:
 		resp, err := lc.client.Do(req)
 		if err != nil {
+			lc.tokens <- token
 			return nil, errors.WithStack(err)
 		}
 		return &Response{
@@ -59,7 +61,7 @@ func (lc *LimitedClient) Do(req *http.Request) (*Response, error) {
 			tokens:   lc.tokens,
 			token:    token,
 		}, nil
-	case <-time.After(lc.timeout):
+	case <-time.After(lc.tokenTimeout):
 		return nil, errors.Errorf("timeout waiting for request token")
 	}
 }

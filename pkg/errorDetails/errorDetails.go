4c4dc8f1cd152fe4bd3498564cd9a96a76a5e2a7
Juergen Enge <you@example.com>
2024-06-20 11:27:30 +0200

    errorcodes added

--- pkg/errorDetails/errorDetails.go
+++ pkg/errorDetails/errorDetails.go
@@ -1,17 +1,42 @@
 package errorDetails
 
-import "emperror.dev/errors"
+import (
+	"context"
+	"emperror.dev/emperror"
+	"emperror.dev/errors"
+	"google.golang.org/appengine/log"
+)
+
+var errorCodes = NewDetailFactory()
 
 type ErrorDetails struct {
-	ErrorCode    int32  `json:"errorCode"`
+	ErrorCode    string `json:"errorCode"`
 	ErrorMessage string `json:"errorMessage"`
 }
 
-func NewErrorDetails(errorCode int32, errorMessage string) *ErrorDetails {
-	return &ErrorDetails{
-		ErrorCode:    errorCode,
-		ErrorMessage: errorMessage,
+func GetErrorStacktrace(err error) errors.StackTrace {
+	type stackTracer interface {
+		StackTrace() errors.StackTrace
 	}
+
+	var stack errors.StackTrace
+
+	errors.UnwrapEach(err, func(err error) bool {
+		e := emperror.ExposeStackTrace(err)
+		st, ok := e.(stackTracer)
+		if !ok {
+			return true
+		}
+
+		stack = st.StackTrace()
+		return true
+	})
+
+	if len(stack) > 2 {
+		stack = stack[:len(stack)-2]
+	}
+	return stack
+	// fmt.Printf("%+v", st[0:2]) // top two frames
 }
 
 func GetDetails(err error) []*ErrorDetails {
@@ -25,38 +50,56 @@ func GetDetails(err error) []*ErrorDetails {
 	return details
 }
 
-func NewDetailFactory() DetailFactory {
-	return DetailFactory{
-		0: "unknown error",
+func GetDetailCodes(err error) []string {
+	var codes []string
+	details := GetDetails(err)
+	for _, d := range details {
+		codes = append(codes, d.ErrorCode)
+	}
+	return codes
+}
+
+func WithDetail(err error, errorCode string) error {
+	detail, ok := GetErrorDetails(errorCode)
+	if !ok {
+		var _err = errors.Errorf("unknown error code: %s", errorCode)
+		log.Debugf(context.Background(), "%v%+v", _err, GetErrorStacktrace(err))
+		emperror.Panic(_err)
+	}
+	return errors.WithDetails(err, detail)
+}
+
+func NewErrorDetails(errorCode string, errorMessage string) *ErrorDetails {
+	return &ErrorDetails{
+		ErrorCode:    errorCode,
+		ErrorMessage: errorMessage,
 	}
 }
 
-type DetailFactory map[int32]string
-
-func (df DetailFactory) SetErrorDetails(errorCode int32, errorMessage string) {
-	df[errorCode] = errorMessage
+func NewDetailFactory() DetailFactory {
+	return DetailFactory{
+		"0000": "unknown error",
+	}
 }
 
-func (df DetailFactory) GetErrorDetails(errorCode int32) (*ErrorDetails, bool) {
-	str, ok := df[errorCode]
+type DetailFactory map[string]string
+
+func SetErrorDetails(errorCode string, errorMessage string) {
+	errorCodes[errorCode] = errorMessage
+}
+
+func GetErrorDetails(errorCode string) (*ErrorDetails, bool) {
+	str, ok := errorCodes[errorCode]
 	if !ok {
 		return nil, false
 	}
 	return NewErrorDetails(errorCode, str), true
 }
 
-func (df DetailFactory) GetErrorDetailsList() []*ErrorDetails {
+func GetErrorDetailsList() []*ErrorDetails {
 	var details = []*ErrorDetails{}
-	for errorCode, errorMessage := range df {
+	for errorCode, errorMessage := range errorCodes {
 		details = append(details, NewErrorDetails(errorCode, errorMessage))
 	}
 	return details
-}
-
-func (df DetailFactory) WithDetail(errorCode int32) error {
-	detail, ok := df.GetErrorDetails(errorCode)
-	if !ok {
-		return nil
-	}
-	return errors.WithDetails(errors.New(""), detail)
 }
